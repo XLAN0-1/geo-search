9533ebcde10e3319b816a85c8432bba8d29ecf72
unknown <lana12olu@gmail.com>
2022-04-08 18:08:53 +0100

    First commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-app.use(express.static(__dirname + "/client/build"));
+app.use(express.static(__dirname + "/build"));
 
 
 //SET UP PORT
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/lient/build/index.html");
+    res.sendFile(__dirname + "/build/index.html");
 })
 
 
